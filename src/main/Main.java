5ce2a96574ae3e9e9c129885cbb8ebc0fcbd2eb2
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2021-03-02 19:53:40 -0300

    Filtrar a lista de arquivos pelo nome que combina com o regex(entrada\d.txt$)

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,16 +10,17 @@ import manager.Files;
 public class Main {
 
 	public static void main(String[] args) {
+		//Pega os arquivos no diretório raíz
 		File[] files =  Files.getFiles("C:\\Users\\lsjsa\\Downloads\\Testes");
 		for(File file : files) {
-			System.out.println(file.getName());
-			try (BufferedReader br = new BufferedReader(new FileReader(file))){
-				String line = br.readLine();
+			System.out.println(file.getName()); //printa o nome do arquivo
+			try (BufferedReader br = new BufferedReader(new FileReader(file))){ //try-catch com iniciação
+				String line = br.readLine(); //ler a linha do arquivo
 				while(line != null) {
-					System.out.println(line);
+					System.out.println(line); // printa a linha
 					line = br.readLine();					
 				}
-				br.close();
+				br.close(); //fecha o arquivo
 				System.out.println("----------------------------------------------------------------------");
 			}catch(IOException e) {
 				System.out.println(e.getMessage());
--- src/manager/Files.java
+++ src/manager/Files.java
@@ -4,14 +4,15 @@ import java.io.File;
 
 public class Files {
 
-	
+	/***
+	 * 
+	 * @param path: O caminho do diretório raíz
+	 * @return A lista de arquivos que seguem a determinação do regex nos seus nomes
+	 */
 	static public File[] getFiles(String path) {
 		File parentPath = new File(path);
-		File[] files = parentPath.listFiles(File::isFile);
+		File[] files = parentPath.listFiles(file -> file.getName().matches("entrada\\d.txt$"));
 		return files;
 	}
 	
-	/*static public File[] inThePatern(String patern) {
-		
-	}*/
 }
