291f468b401a65578d9fea43ff6ac5504c9252d3
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2021-05-15 14:42:09 -0300

    Parte inicial de tratamento de erros sintáticos, no bloco const

--- src/lexicAnalyser/Word.java
+++ src/lexicAnalyser/Word.java
@@ -11,9 +11,35 @@ public class Word extends Token{
 	public String toString() {
 		return "<" + super.line + " " + super.getSymbol() + " " + lexeme + ">\n"; 
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((lexeme == null) ? 0 : lexeme.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		if (lexeme == null) {
+			if (other.lexeme != null)
+				return false;
+		} else if (!lexeme.equals(other.lexeme))
+			return false;
+		return true;
+	}
 	
 	/*public final Word and = new Word ("&&", Tag.LOG),
 			or = new Word("||", Tag.LOG), eq = new Word("==", Tag.LOG),
 			dif = new Word("!=", Tag.LOG), le = new Word("<=", Tag.LOG),
 			ge = new Word(">=", Tag.LOG); */
+	
 }
